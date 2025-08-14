4099f3d9053c27f1958bb6dd96e3be3153718efa
kunyoungshin <kunyoungsin@naver.com>
2025-08-14 17:57:54 +0900

    servlet-context1

--- Day1/src/main/java/com/nhnacademy/hello/MultiServlet.java
+++ Day1/src/main/java/com/nhnacademy/hello/MultiServlet.java
@@ -15,8 +15,10 @@ public class MultiServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String[] values = req.getParameterValues("class");
+        String url = getServletContext().getInitParameter("url");
         try (PrintWriter out = resp.getWriter()) {
             out.println(String.join(",", values));
+            out.printf("url:%s\n", url);
         } catch (IOException ex) {
             log.info(ex.getMessage());
         }
