3174a49087a23c90983003ce4531ea28232d9f16
kunyoungshin <kunyoungsin@naver.com>
2025-08-14 16:41:09 +0900

    logback 설정추가

---
+++ Day1/src/main/java/com/nhnacademy/Main.java
@@ -0,0 +1,10 @@
+package com.nhnacademy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Hellow World!");
+    }
+}
