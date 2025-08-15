ab95dc6f8362ca08bcdd82e76a72e9bf7ab3f446
kunyoungshin <kunyoungsin@naver.com>
2025-08-15 20:10:14 +0900

    filter

---
+++ Day1/src/main/java/com/nhnacademy/hello/filter/CharacterEncodingFilter.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.hello.filter;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class CharacterEncodingFilter implements Filter {
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.encoding = filterConfig.getInitParameter("encoding");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(this.encoding);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
+
