d2f670d179f466056a57ed5458d1e0e8fda55091
kunyoungshin <kunyoungsin@naver.com>
2025-08-15 13:11:00 +0900

    cookie

---
+++ Day1/src/main/java/com/nhnacademy/util/CookieUtils.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public final class CookieUtils {
+
+    private CookieUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static Cookie getCookie(HttpServletRequest req, String name) {
+        return Optional.ofNullable(req.getCookies())
+                .flatMap(cookies -> Arrays.stream(cookies)
+                        .filter(c -> c.getName().equals(name))
+                        .findFirst())
+                .orElse(null);
+    }
+}
